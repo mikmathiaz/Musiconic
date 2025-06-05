bcea0c4aae5f62c9cd5bb93f2812f520d99baca3
mikmathiaz <mikaelmssl@gmail.com>
2025-06-05 20:27:35 -0300

    mudança de tela main e evolução do app

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,52 @@
 import 'package:flutter/material.dart';
-import 'TelaPrincipal.dart';
+import 'package:musiconic/pages/home_page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MusiconicApp());
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class MusiconicApp extends StatelessWidget {
+  const MusiconicApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-   return Center(
-     child: Screen(),
-   );
+    return MaterialApp(
+      title: 'Musiconic',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: const Color(0xFF1C1C2D),
+        colorScheme: ColorScheme.dark(
+          primary: Colors.purpleAccent,
+          secondary: Colors.deepPurpleAccent,
+        ),
+        textTheme: const TextTheme(bodyMedium: TextStyle(color: Colors.white)),
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const HomePage()),
+      );
+    });
+
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Musiconic',
+          style: TextStyle(
+            color: Colors.purpleAccent,
+            fontSize: 42,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Montserrat',
+          ),
+        ),
+      ),
+    );
   }
 }
